3473f45cb44e35ce356569639d8556ae4e8a7152
Sergii Lovygin <sergey.lovygin@yahoo.com>
2021-07-28 11:02:59 +0300

    add static asserts to make error messages more clear and earlier;

--- wheels/random/random.hpp
+++ wheels/random/random.hpp
@@ -2,18 +2,23 @@
 #define WHEELS_RANDOM_RANDOM_HPP
 
 #include <random>
+#include <type_traits>
 
 
 namespace wheels {
     namespace random {
         template<typename Int, typename RandomGenerator>
         Int random(Int min_value, Int max_value, RandomGenerator& generator) {
+            static_assert(std::is_integral<Int>::value, "Function supports only integral types");
+
             std::uniform_int_distribution<Int> distribution(min_value, max_value);
             return distribution(generator);
         }
 
         template<typename Int>
         Int random(Int min_value, Int max_value) {
+            static_assert(std::is_integral<Int>::value, "Function supports only integral types");
+
             static std::random_device rand_device;
             static std::mt19937 generator(rand_device());
             return random(min_value, max_value, generator);
